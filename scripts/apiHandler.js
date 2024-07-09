1bcb0e46cd25e66ee15d55bce17f24b01217f1ef
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2024-07-09 12:44:11 -0300

    set base of version

--- scripts/apiHandler.js
+++ scripts/apiHandler.js
@@ -1,17 +1,33 @@
-export let subraces;
-export let subclasses;
+export let races;
+export let classes;
 
-//const API_URL = "http://127.0.0.1:5000/";
 const API_URL = "https://jgss-web-service.onrender.com/hnm/";
 
-export async function getData(){
-    let response = await fetch(API_URL+"query-subclasses");
-    subclasses = await response.json();
+export async function getGameData(){
+    let response = await fetch(API_URL+"query-classes");
+    classes = await response.json();
 
-    response = await fetch(API_URL+"query-subraces");
-    subraces = await response.json();
+    response = await fetch(API_URL+"query-races");
+    races = await response.json();
 
-    console.log(subclasses);
-    console.log(subraces);
+    // Criar uma série de requisições para cada classe
+    let classRequests = classes.map(className => fetch(API_URL + "query-" + className));
+    let racesRequests = races.map(racesName => fetch(API_URL + "query-" + racesName));
 
+    // Esperar todas as requisições serem completadas
+    let classResponses = await Promise.all(classRequests);
+    let racesResponses = await Promise.all(racesRequests);
+
+    // Processar as respostas das requisições
+    let classData = await Promise.all(classResponses.map(response => response.json()));
+    let racesData = await Promise.all(racesResponses.map(response => response.json()));
+
+    return { 
+        classes: classData,
+        races: racesData
+    }
+};
+
+export async function getCharacterData(){
+    ;
 };
--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,22 @@
-import { getData } from "./apiHandler.js";
+import { getCharacterData, getGameData } from "./apiHandler.js";
 
-getData();
+const game = await getGameData();
+let character = await getCharacterData();
+
+function loadMainContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const mainElement = document.createElement('main');
+            mainElement.innerHTML = data;
+
+            const body = document.querySelector('body');
+            body.appendChild(mainElement);
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o conteúdo:', error);
+        });
+}
+loadMainContent();
+
+console.log(game)
