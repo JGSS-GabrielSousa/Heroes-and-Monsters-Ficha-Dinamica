10aaa9962bed2a4a32cc2a79f989f7487f60525f
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2024-08-02 14:23:01 -0300

    prevent bug when html is not loaded

--- scripts/utils.js
+++ scripts/utils.js
@@ -58,12 +58,124 @@ export function accentsTidy(string){
 }
 
 export function loadHTMLContent(url, selector) {
-    fetch("./views/" + url)
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector(selector).innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Erro ao carregar o conteúdo: ', error);
-        });
+    setTimeout(e => {
+        fetch("./views/" + url)
+            .then(response => response.text())
+            .then(data => {
+                document.querySelector(selector).innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Erro ao carregar o conteúdo: ', url);
+
+                setTimeout(e => {
+                    loadHTMLContent(url, selector);
+                }, rand(0,1000))
+            });
+    }, rand(0,1000));
+}
+
+
+export function rollDice(diceString) {
+    
+    function rollDiceFunc(diceString){
+        function diceRoll(sides) {
+            return Math.floor(Math.random() * sides) + 1;
+        }
+
+        function rollSingleDiceExpression(expression) {
+            const regex = /(\d*)d(\d+)(\+\d+)?/;
+            const matches = expression.match(regex);
+            const roll = [];
+            roll.subroll = [];
+
+            if (matches) {
+                const count = matches[1] ? parseInt(matches[1]) : 1; // Quantidade de dados
+                const sides = parseInt(matches[2]); // Lados do dado
+                const modifier = matches[3] ? parseInt(matches[3]) : 0; // Modificador adicional
+
+                let total = 0;
+
+                for (let i = 0; i < count; i++) {
+                    const rollValue = diceRoll(sides);
+                    
+                    const subroll = {
+                        roll: 0,
+                        sides: 0,
+                    }
+
+                    subroll.sides = sides;
+                    subroll.roll = rollValue;
+
+                    roll.subroll.push(subroll);
+
+                    total += rollValue;
+                }
+
+                roll.expression = matches.input;
+                roll.modifier = modifier;
+                roll.total = total+modifier;
+
+                total += modifier;
+
+                return roll;
+
+            } else {
+                throw new Error("Invalid dice expression format");
+            }
+        }
+
+        const diceExpressions = diceString.split(' + ');
+
+        const results = diceExpressions.map(rollSingleDiceExpression);
+
+        return {results, total: results.reduce((total, roll) => total + roll.total, 0)};
+    }
+
+    const damageString = "d20"+diceString;
+    const roll = rollDiceFunc(damageString);
+    let html = ``;
+    html += `<table class="roll-table">`
+
+    roll.results.forEach(result => {
+        
+        html += `
+        <tr>
+            <td>${result.expression}:</td>
+            <td>(
+        `
+
+        for (let i = 0; i < result.subroll.length; i++) {
+            const r = result.subroll[i];
+            
+            html += `
+                ${r.roll}
+            `
+
+            if(i+1 < result.subroll.length){
+                html += ` + `
+            }
+            else if(result.modifier != 0) {
+                html += `) + ${result.modifier}`
+            }
+            else {
+                html += `)`
+            }
+        }
+
+        html += `
+                <td>=</td>
+                <td><b>${result.total}</b></td>
+            <tr>
+        `
+    });
+
+
+    Swal.fire({
+        html: `
+            ${html}
+            <h1>${roll.total}</h1>
+        `,
+        showCloseButton: true,
+        focusConfirm: false,
+    });
 }
